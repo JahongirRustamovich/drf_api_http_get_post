5fc1c52c93d328200682990d8ea35192ad8d7b1a
JahongirRustamovich <153514812+JahongirRustamovich@users.noreply.github.com>
2025-06-15 22:51:58 +0500

    drf_api_http_get_delete
    drf_api_http_(get_delete)

--- drf_api_learning/api/urls.py
+++ drf_api_learning/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ProductListCreateView
+from .views import ProductDetailAPIView
 
 urlpatterns = [
-    path('products/', ProductListCreateView.as_view(), name='product-list'),
+    path('products/<int:pk>/', ProductDetailAPIView.as_view(), name='product-detail'),
 ]
+
 
 
--- drf_api_learning/api/views.py
+++ drf_api_learning/api/views.py
@@ -1,9 +1,24 @@
-from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 from .models import Product
 from .serializers import ProductSerializer
 
-class ProductListCreateView(generics.ListCreateAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
+class ProductDetailAPIView(APIView):
+    def get(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    def delete(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+        product.delete()
+        return Response({"message": "Mahsulot o‘chirildi."}, status=status.HTTP_204_NO_CONTENT)
+
+
+
+
+
 
 
