64c3183a01813c7fdf3d2a09a4e4a0dcba43090e
JahongirRustamovich <153514812+JahongirRustamovich@users.noreply.github.com>
2025-06-16 13:59:52 +0500

    drf_api_learning

--- drf_api_learning/my_api/views.py
+++ drf_api_learning/my_api/views.py
@@ -3,6 +3,12 @@ from .models import Product
 from .serializers import ProductSerializer
 
 class ProductViewSet(ModelViewSet):
-    queryset = Product.objects.all()
+    queryset = Product.objects.all().order_by('-id')
+    # queryset = Product.objects.all() # <-- agar tartib muhim bulmasa
     serializer_class = ProductSerializer
+
+
+
+
+
 
